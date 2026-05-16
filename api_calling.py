027a54a89d28983c85310249c9960072c9277866
Yasir Fuad <mdyasirfuad49@gmail.com>
2026-05-16 13:23:04 +0600

    Add files via upload

---
+++ api_calling.py
@@ -0,0 +1,19 @@
+from google import genai
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+my_api_key = os.getenv("GEMINI_API_KEY")
+# initailizing a client
+client = genai.Client(api_key= my_api_key)
+# note generator 
+def ans_generator(text):
+    prompt = """Give a short description of the question . Answer the question.
+    make sure to add necessary markdown to 
+    differenciate different section. everything is in 100 words max."""
+
+    response = client.models.generate_content(
+        model = "gemini-3-flash-preview",
+        contents=[text, prompt])
+    
+    return response.text
---
+++ problem-5.py
@@ -0,0 +1,35 @@
+# Task 5: Gemini Chatbot App
+
+
+# একটা simple Streamlit app বানাও যেখানে user একটা question type করবে এবং Gemini API সেটার answer দেবে।
+
+# Requirements
+# st.text_input() দিয়ে user-এর question নাও
+# একটা Ask Gemini button রাখো
+# Button click করলে Gemini API-তে question পাঠাও এবং response st.markdown() দিয়ে দেখাও
+# .env file থেকে API key load করো — directly code-এ লেখা যাবে না
+
+
+import streamlit as st
+from api_calling import ans_generator
+
+text = st.text_input("", placeholder="Write your question here")
+
+button = st.button("Ask Gemini", type='primary')
+
+if button:
+    if not text:
+        st.error("Write question first")
+
+    else:
+        try:
+            with st.spinner("Generating answer"):
+                gen_answer = ans_generator(text)
+
+        except Exception as e:
+            st.error(f"Something went wrong: {e}")
+
+        else:
+            with st.container(border=True):
+                st.markdown(gen_answer)            
+        
